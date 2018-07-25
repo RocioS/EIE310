7ec326f097bf0ee6a8396bb3101255fdf399b91b
RocioS <37635092+RocioS@users.noreply.github.com>
2018-07-25 17:54:39 -0300

    Add files via upload

--- Ejercicio4.m
+++ Ejercicio4.m
@@ -1,8 +1,30 @@
-a=[1,-1,0.9];
-b=1;
-[x,n]=impulso(0,-20,100);
-h=filter(b,a,x);
-[y,m]=escalon(0,-20,100);
-g=filter(b,a,y);
-subplot 121; plot(n,h);title('Respuesta impulso');grid
-subplot 122; plot(m,g);title('Respuesta escalon');grid
+load('ecg_orig.txt')
+load('ecg_iir.txt')
+load('ecg_fir.txt')
+n1 = 1400; % n inicial
+n2 = 1800; % n final
+n = (n1 : n2);
+load('ecg_orig.txt')
+load('ecg_iir.txt')
+load('ecg_fir.txt')
+n1 = 1400; % n inicial
+n2 = 1800; % n final
+n = (n1 : n2);
+t = n / 200; % [seg]
+t1 = n1 / 200; % t inicial
+t2 = n2 / 200; % t final
+subplot 311;
+plot(t, ecg_orig(n));
+axis([t1 t2 -0.5 1.5]);
+xlabel('seg'); grid
+title('ECG original');
+subplot 312;
+plot(t, ecg_iir(n));
+axis([t1 t2 -0.5 1.5]);
+xlabel('seg'); grid
+title('Salida IIR');
+subplot 313;
+plot(t, ecg_fir(n));
+axis([t1 t2 -0.5 1.5]);
+xlabel('seg'); grid
+title('Salida FIR');
